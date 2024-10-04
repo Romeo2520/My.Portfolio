1d26f45660a9c2ba96d79a37628f38e3c85e41f1
Romeo2520 <romeo.gossou-bah@epitech.eu>
2024-10-04 19:42:07 +0100

    My PortFolio

---
+++ src/data/HeroIconsData.js
@@ -0,0 +1,35 @@
+//Only designed to support 5 icons.
+//If you want to add more. youll need to mify styles
+//Every class (example: icon-4), must be styled to preserve responsive
+export const HeroIconsSocial = [
+    {
+        name:"github",
+        class: "icon-1",
+        url: "https://github.com/Romeo2520",
+        src: "./svg/github-light.svg"
+    },
+    {
+        name:"X",
+        class: "icon-2",
+        url: "https://dribbble.com/GOSSOU",
+        src: "https://img.icons8.com/?size=100&id=LULpAxzosrfL&format=png&color=000000"
+    },
+    {
+        name:"linkedin",
+        class: "icon-3",
+        url: "https://www.linkedin.com/in/rom%C3%A9o-gossou-bah-46aa21234/",
+        src: "./svg/linkedin.svg"
+    },
+    {
+        name:"whatsapp",
+        class: "icon-4",
+        url: "https://wa.me//97333745",
+        src: "https://img.icons8.com/?size=100&id=DUEq8l5qTqBE&format=png&color=000000"
+    },
+    {
+        name:"Instagram",
+        class: "icon-5",
+        url: "https://www.instagram.com/goss.romeo?igsh=N211N2RoNWM4YnAy&utm_source=qr",
+        src: "https://img.icons8.com/3d-fluency/94/instagram-new.png"
+    },
+]
